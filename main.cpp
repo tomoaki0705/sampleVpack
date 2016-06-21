8e11213babec87e31827cd45bdb1721de980070c
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2016-06-22 08:28:56 +0900

    implement non-vector part

--- main.cpp
+++ main.cpp
@@ -42,8 +42,47 @@ inline int8x16_t cv_vpack_s16(int16x8_t v0, int16x8_t v1)
 	return vcombine_s8(vmovn_s16(v0), vmovn_s16(v1));
 }
 
+void fill(uint32_t* ptr, RNG& r)
+{
+	for(uint32_t i = 0;i < 4;i++)
+		ptr[i] = (r.next() & 0x7fff);
+}
+
+void debugPack(const uint32_t* src0, const uint32_t* src1, uint16_t* dst)
+{
+	for(uint32_t i = 0;i < (16/sizeof(uint32_t));i++)
+	{
+		dst[i*2  ] = (uint16_t)(src0[i]);
+		dst[i*2+1] = (uint16_t)(src1[i]);
+	}
+}
+
 int main(int argc, char** argv)
 {
 	RNG r(0x123);
+	uint32_t src0[4], src1[4];
+	uint16_t dst[8];
+	fill(src0, r);
+	fill(src1, r);
+	debugPack((const uint32_t*)src0, (const uint32_t*)src1, (uint16_t*)dst);
+	std::cout << "src0:";
+	for(uint32_t i = 0;i < 4;i++)
+	{
+		std::cout << '\t' << src0[i];
+	}
+	std::cout << std::endl;
+	std::cout << "src1:";
+	for(uint32_t i = 0;i < 4;i++)
+	{
+		std::cout << '\t' << src1[i];
+	}
+	std::cout << std::endl;
+	std::cout << "dst :";
+	for(uint32_t i = 0;i < 8;i++)
+	{
+		std::cout << '\t' << dst[i];
+	}
+	std::cout << std::endl;
+
 	return 0;
 }
