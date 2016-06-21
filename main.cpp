939e5eff0d759497cd2e1e438b98e77ad15c151c
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2016-06-22 08:44:45 +0900

    add more templated implementation

--- main.cpp
+++ main.cpp
@@ -42,18 +42,21 @@ inline int8x16_t cv_vpack_s16(int16x8_t v0, int16x8_t v1)
 	return vcombine_s8(vmovn_s16(v0), vmovn_s16(v1));
 }
 
-void fill(uint32_t* ptr, RNG& r)
+template <typename ST>
+void fill(ST* ptr, RNG& r)
 {
-	for(uint32_t i = 0;i < 4;i++)
-		ptr[i] = (r.next() & 0x7fff);
+	unsigned int cLoop = 16/sizeof(ST);
+	for(unsigned int i = 0;i < cLoop;i++)
+		ptr[i] = (r.next() & 0x7f);
 }
 
-void debugPack(const uint32_t* src0, const uint32_t* src1, uint16_t* dst)
+template <typename ST, typename DT>
+void debugPack(const ST* src0, const ST* src1, DT* dst)
 {
-	for(uint32_t i = 0;i < (16/sizeof(uint32_t));i++)
+	for(uint32_t i = 0;i < (16/sizeof(ST));i++)
 	{
-		dst[i*2  ] = (uint16_t)(src0[i]);
-		dst[i*2+1] = (uint16_t)(src1[i]);
+		dst[i*2  ] = (DT)(src0[i]);
+		dst[i*2+1] = (DT)(src1[i]);
 	}
 }
 
@@ -83,15 +86,21 @@ void dumpArray(const ST* src0, const ST* src1, const DT* dst)
 	cout << endl;
 }
 
+template <typename ST, typename DT>
+void verifyArray(ST* src0, ST* src1, DT* dst, RNG& r)
+{
+	fill(src0, r);
+	fill(src1, r);
+	debugPack((const ST*)src0, (const ST*)src1, (DT*)dst);
+	dumpArray(src0, src1, dst);
+}
+
 int main(int argc, char** argv)
 {
 	RNG r(0x123);
 	uint32_t src0[4], src1[4];
 	uint16_t dst[8];
-	fill(src0, r);
-	fill(src1, r);
-	debugPack((const uint32_t*)src0, (const uint32_t*)src1, (uint16_t*)dst);
-	dumpArray(src0, src1, dst);
+	verifyArray(src0, src1, dst, r);
 
 	return 0;
 }
